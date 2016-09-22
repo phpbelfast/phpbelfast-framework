a061f0808493e596c3d7ccbd62009ac7a8455013
andy <andy@thegrahams.info>
2016-09-22 16:44:26 +0100

    some tweaking for demo

--- views/layout.twig.php
+++ views/layout.twig.php
@@ -19,7 +19,7 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="{{ urlFor('posts.index') }}">Articles</a></li>
+                <li><a href="{{ urlFor('posts.index') }}">Posts</a></li>
                 <li><a href="{{ urlFor('events.index') }}">Events</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">URLs <span class="caret"></span></a>
